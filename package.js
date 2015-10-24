cfddf83d36bb6540773ae5870c90022352873c13
mcbrumagin <mcbrumagin@gmail.com>
2015-10-24 14:52:14 -0400

    Mongo version added

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
     summary: 'Client & Server events',
-    version: '0.0.1',
+    version: '0.0.2',
     name: 'mcbrumagin:meteor-events',
     git: 'https://github.com/mcbrumagin/meteor-events.git'
 })
 
 Package.onUse(function(api) {
-    api.use(['mongo'], ['client', 'server'])
+    api.use(['mongo@1.0.10'], ['client', 'server'])
     api.use(['coffeescript@1.0.10'], ['client', 'server'])
     api.addFiles('events.coffee')
     api.export('Events', ['client', 'server'])
